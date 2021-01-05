7dd424a46187a7bd2414463a7a31cb6052ab404a
Georgios A. Tsakalos <me@ozmax.eu>
2021-01-05 02:23:38 +0200

    ls color update

--- zsh_configs/zsh/colors.zsh
+++ zsh_configs/zsh/colors.zsh
@@ -6,13 +6,13 @@ autoload -Uz colors && colors
 LS_COLORS='di=1;34:'
 
 #file color
-LS_COLORS=$LS_COLORS:'fi=4;93:'
+LS_COLORS=$LS_COLORS:'fi=0;93:'
 
 # symlink color
 LS_COLORS=$LS_COLORS:'ln=0;36:'
 
 # executable color
-LS_COLORS=$LS_COLORS:'ex=0;31:'
+LS_COLORS=$LS_COLORS:'ex=1;31:'
 export LS_COLORS
 
 #################
